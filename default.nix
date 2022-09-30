c4e7a0ce74d8bc371a08dfd227c4b81f8be81256
teodanciu <teodora.danciu@tweag.io>
2022-09-30 16:26:45 +0100

    Simplify definitions of nix derivations
    and unify the naming for consistency

--- default.nix
+++ default.nix
@@ -1,6 +1,10 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-with nixpkgs; let
-  agdaDeps = with (import (builtins.fetchTarball
+{
+  pkgs ? import <nixpkgs> {},
+}:
+
+with pkgs;
+let
+  customAgda = (import (builtins.fetchTarball
     https://github.com/nixos/nixpkgs/tarball/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31) ({ # 21.11
       overlays = [ (import (fetchFromGitHub {
         repo = "agda";
@@ -8,11 +12,11 @@ with nixpkgs; let
         rev = "d5ea03b96328f38741efef4535197076ff0e05d5";
         sha256 = "WiadZWZvPWcR49JkiXPiMKW3plRjBlR94wyg/aoEoG8=";
       })).overlay ];
-    } // (if pkgs.system == "aarch64-darwin" then { system = "x86_64-darwin"; } else {}))); let
+    } // (if pkgs.system == "aarch64-darwin" then { system = "x86_64-darwin"; } else {})));
 
-    agda-stdlib = agdaPackages.standard-library.overrideAttrs (oldAttrs: {
+  agdaStdlib = customAgda.agdaPackages.standard-library.overrideAttrs (oldAttrs: {
       version = "1.7";
-      src =  fetchFromGitHub {
+      src =  customAgda.fetchFromGitHub {
         repo = "agda-stdlib";
         owner = "input-output-hk";
         rev = "f8fdb925c74e8d3b0c88e2a5520bc11e606d34c6";
@@ -20,57 +24,56 @@ with nixpkgs; let
       };
     });
 
-    agda-finset = agdaPackages.mkDerivation {
-      pname = "agda-finset";
-      version = "0.9";
-      src = fetchFromGitHub {
-        repo = "agda-finset";
-        owner = "input-output-hk";
-        rev = "939b2578f4f8cb4f02928b30edfc787cabeba171";
-        sha256 = "2bUMmUikzNRaEFVkH+Y8ypnNF65d/LNKei6fSJ982AY=";
-      };
-      meta = {};
-      libraryFile = "Finset.agda-lib";
-      everythingFile = "src/README.agda";
-      buildInputs = [ agda-stdlib ];
+  agdaFinset = customAgda.agdaPackages.mkDerivation {
+    pname = "agda-finset";
+    version = "0.9";
+    src = customAgda.fetchFromGitHub {
+      repo = "agda-finset";
+      owner = "input-output-hk";
+      rev = "939b2578f4f8cb4f02928b30edfc787cabeba171";
+      sha256 = "2bUMmUikzNRaEFVkH+Y8ypnNF65d/LNKei6fSJ982AY=";
     };
-
-    agda-stdlib-meta = agdaPackages.mkDerivation {
-      pname = "agda-stdlib-meta";
-      version = "0.1";
-      src = fetchFromGitHub {
-        repo = "stdlib-meta";
-        owner = "omelkonian";
-        rev = "dadb6a468b9cdc47442b48a47b848f8e8fbffda7";
-        sha256 = "YkUtM5Gos6xd7ZsZPqcuVy6DZqNA7n/exPfQngir+y0=";
-      };
-      patches = [ ./stdlib-meta-update-imports.patch ];
-      meta = {};
-      libraryFile = "stdlib-meta.agda-lib";
-      everythingFile = "Main.agda";
-      buildInputs = [ agda-stdlib ];
-    };
-
-    deps = [ agda-stdlib agda-finset agda-stdlib-meta ];
-  in {
-    agda = agda.withPackages { pkgs = deps; ghc = nixpkgs.ghc; };
-    ledger = agdaPackages.mkDerivation {
-      pname = "Agda-ledger";
-      version = "0.1";
-      src = ./src;
-      meta = {};
-      buildInputs = deps;
-      postInstall = "cp -r latex $out";
-      extraExtensions = [ "hs" "cabal" ];
-    };
+    meta = {};
+    libraryFile = "Finset.agda-lib";
+    everythingFile = "src/README.agda";
+    buildInputs = [ agdaStdlib ];
   };
-  agda-ledger = agdaDeps.ledger;
-  agdaWithPkgs = agdaDeps.agda;
 
-  ledger-hs-src = stdenv.mkDerivation {
+  agdaStdlibMeta = customAgda.agdaPackages.mkDerivation {
+    pname = "agda-stdlib-meta";
+    version = "0.1";
+    src = customAgda.fetchFromGitHub {
+      repo = "stdlib-meta";
+      owner = "omelkonian";
+      rev = "dadb6a468b9cdc47442b48a47b848f8e8fbffda7";
+      sha256 = "YkUtM5Gos6xd7ZsZPqcuVy6DZqNA7n/exPfQngir+y0=";
+    };
+    patches = [ ./stdlib-meta-update-imports.patch ];
+    meta = {};
+    libraryFile = "stdlib-meta.agda-lib";
+    everythingFile = "Main.agda";
+    buildInputs = [ agdaStdlib ];
+  };
+
+  deps = [ agdaStdlib agdaFinset agdaStdlibMeta ];
+  agdaWithPkgs = customAgda.agda.withPackages { pkgs = deps; ghc = pkgs.ghc; };
+
+in rec {
+
+  agdaLedger = customAgda.agdaPackages.mkDerivation {
+    pname = "Agda-ledger";
+    version = "0.1";
+    src = ./src;
+    meta = {};
+    buildInputs = deps;
+    postInstall = "cp -r latex $out";
+    extraExtensions = [ "hs" "cabal" ];
+  };
+
+  ledgerHsSrc = stdenv.mkDerivation {
     pname = "Agda-ledger-hs-src";
     version = "0.1";
-    src = "${agda-ledger}";
+    src = "${agdaLedger}";
     meta = {};
     buildInputs = [ agdaWithPkgs ];
     buildPhase = "";
@@ -83,15 +86,13 @@ with nixpkgs; let
       find $out/MAlonzo -name "*.hs" -print | sed "s#^$out/#        #;s#\.hs##;s#/#.#g" >> $out/agda-ledger-executable-spec.cabal
     '';
   };
-in {
-  agda = agdaWithPkgs;
-  agda-ledger = agda-ledger;
-  agda-ledger-executable-spec = haskellPackages.callCabal2nix "agda-ledger-executable-spec" "${ledger-hs-src}" {};
 
-  ledger-docs = stdenv.mkDerivation {
+  ledgerExecutableSpec = haskellPackages.callCabal2nix "Agda-ledger-executable-spec" "${ledgerHsSrc}" {};
+
+  ledgerDocs = stdenv.mkDerivation {
     pname = "simple-utxo-ledger-docs";
     version = "0.1";
-    src = "${agda-ledger}";
+    src = "${agdaLedger}";
     meta = {};
     buildInputs = [ agdaWithPkgs (texlive.combine {
       inherit (texlive)
