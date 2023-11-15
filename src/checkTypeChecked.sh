fd39ea8b32b1cd0cafb7873560df53fea5cfad32
Orestis Melkonian <orestis.melkonian@iohk.io>
2023-11-15 17:09:19 +0000

    CI: record typechecking times

--- src/checkTypeChecked.sh
+++ src/checkTypeChecked.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-checkTypeChecked()
-{
-res=0
-
-for file in $(find . -name '*.agda')
-do
-    if ! [ -f "${file%.agda}.agdai" ]; then
-        echo "${file%.agda}.agdai doesn't exist"
-        res=1
-    fi
+echo "Checking that all Agda files have been typechecked..."
+for agdaFn in $(find . -name '*.*agda'); do
+  agdaiFn="${agdaFn%.*agda}.agdai"
+  if ! [ -f $agdaiFn ]; then
+    echo "        FAIL: $agdaiFn doesn't exist"
+    exit 1
+  fi
 done
-
-for file in $(find . -name '*.lagda')
-do
-    if ! [ -f "${file%.lagda}.agdai" ]; then
-        echo "${file%.lagda}.agdai doesn't exist"
-        res=1
-    fi
-done
-
-return $res
-}
-
-checkTypeChecked
+echo "        PASS: all Agda files have been typechecked"
