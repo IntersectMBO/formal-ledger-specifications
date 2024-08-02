635bc18badd687d8e7e410646a77f8223f435fcf
Lucsanszky <daniel.lucsanszky@iohk.io>
2024-08-02 02:46:29 +0200

    Init

--- src/Ledger/Set.agda
+++
@@ -1,6 +0,0 @@
-{-# OPTIONS --safe #-}
-
-module Ledger.Set where
-
-open import Ledger.Set.Theory public
-open import Ledger.Set.HashMap public
--- src/MidnightExample/hs-src/Lib.hs
+++
@@ -1,7 +0,0 @@
-module Lib
-  ( module MAlonzo.Code.MidnightExample.HSLedger
-  , module MAlonzo.Code.MidnightExample.Types
-  ) where
-
-import MAlonzo.Code.MidnightExample.HSLedger
-import MAlonzo.Code.MidnightExample.Types
