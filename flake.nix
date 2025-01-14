78aa498570a1d562c57aa8084aab1f9814df2642
Robin Stumm <serverkorken@gmail.com>
2025-01-14 22:06:52 +0100

    add cache to flake nix config

--- flake.nix
+++ flake.nix
@@ -33,4 +33,13 @@
       };
     });
   };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
 }
