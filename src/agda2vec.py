2a69366e496d2f229594ee192562c3e7b099c38c
William DeMeo <williamdemeo@gmail.com>
2025-01-31 07:52:29 -0700

    agda2vec introduces extra empty lines #653
    This closes issue #653.
    
    (It may be as simple as removing the extra new line character `+ \n` from the `file.write` line in the `write_file` function.)

--- src/agda2vec.py
+++ src/agda2vec.py
@@ -4,7 +4,7 @@ import sys
 def write_file(filename, lines):
     with open(filename, 'w') as file:
         for line in lines:
-            file.write(line + '\n')
+            file.write(line)
 
 def read_file(filename):
     with open(filename, 'r') as file:
