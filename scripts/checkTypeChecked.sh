be33025d7df6ee8ac3c1adfd8cabd1e1443f9bc9
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-03-25 17:40:18 +0100

    Add shellcheck CI (#729)
    - Remove unused script
    - Refactor checkTypeChecked.sh (addresses #717)

--- scripts/checkTypeChecked.sh
+++ scripts/checkTypeChecked.sh
@@ -1,15 +1,47 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-AGDA_VER=$(agda --numeric-version)
+set -o errexit
+set -o nounset
+set -o noglob
+set -o pipefail
 
-echo "Checking that all Agda files have been typechecked..."
-for agdaFn in $(find src -name '*.*agda'); do
+AGDA_VER="$(agda --numeric-version)"
+
+# read all agda source files
+readarray -d '' allAgdaFiles < <(find src -name "*.*agda" -print0)
+
+# temporary variables
+unusedAgdaFiles=() # files without a corresponding iface file
+uncheckedAgdaFiles=() # files whose corresponding iface file is older
+
+printf "Checking that all Agda files have been typechecked.\n"
+
+for agdaFn in "${allAgdaFiles[@]}"; do
   agdaiFn="_build/$AGDA_VER/agda/${agdaFn%.*agda}.agdai"
-  if [ "$(( $(stat -c "%Y" $agdaiFn) - $(stat -c "%Y" $agdaFn) ))" -lt "0" ]; then
-    if [ "$1" = "-m" ]; then
-      echo "        FAIL: $agdaiFn does not exist. Please remove the corresponding agda file or import it somewhere, e.g. from the Everything module."
+  # check if the corresponding iface file exists
+  if [ -f "${agdaiFn}" ]; then
+    # check if the iface file is older than the src file
+    if [ "$(( $(stat -c "%Y" "${agdaiFn}") - $(stat -c "%Y" "${agdaFn}") ))" -lt "0" ]; then
+      uncheckedAgdaFiles+=("${agdaFn}")
     fi
-    exit 1
+  else
+    unusedAgdaFiles+=("${agdaFn}")
   fi
 done
-echo "        PASS: all Agda files have been typechecked."
+
+if [ ${#unusedAgdaFiles[*]} -ne 0 ] || [ ${#uncheckedAgdaFiles[*]} -ne 0 ]; then
+  printf "Agda files without corresponding interface file:\n"
+  for agdaFn in "${unusedAgdaFiles[@]}"; do
+    printf "  %s\n" "${agdaFn}"
+  done
+
+  printf "Agda files whose interface file is older:\n"
+  for agdaFn in "${uncheckedAgdaFiles[@]}"; do
+    printf "  %s\n" "${agdaFn}"
+  done
+
+  printf "Please remove the corresponding agda file or import it somewhere, e.g. from the Everything module.\n"
+  exit 1
+fi
+
+printf "PASS: all Agda files have been typechecked.\n"
