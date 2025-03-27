8ccb1f30da41c3ee2c0be6237fe3307cd8030bf0
William DeMeo <william.demeo@iohk.io>
2025-03-26 21:00:36 -0600

    Fix fls-shake.nix to avoid missing buildHaskellPackage

--- fls-shake.nix
+++ fls-shake.nix
@@ -1,6 +1,18 @@
+### fls-shake.nix
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.haskellPackages.callPackage (pkgs.runCommand "fls-shake" {
+  pname = "fls-shake";
+  version = "0.1.0.0";
+
+  src = ./.;
+
+  isLibrary = false;
+  isExecutable = true;
+
+  executableHsModules = [ "Shakefile.hs" ];
+  mainModule = "Shakefile.hs";
+
   buildInputs = with pkgs.haskellPackages; [
     ghc
     shake
@@ -12,3 +24,4 @@ pkgs.haskellPackages.callPackage (pkgs.runCommand "fls-shake" {
   mkdir -p $out/bin
   ghc -o $out/bin/fls-shake Shakefile.hs -threaded
 '') {}
+
