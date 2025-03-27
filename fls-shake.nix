1dacd56ee97ee860e542ffa92f14ffcd8d6f2b42
William DeMeo <william.demeo@iohk.io>
2025-03-26 21:04:07 -0600

    Fix: use ghcWithPackages for fls-shake

--- fls-shake.nix
+++ fls-shake.nix
@@ -1,18 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  ghcEnv = pkgs.haskellPackages.ghcWithPackages (hp: [
+    hp.shake
+    hp.hashable
+    hp.binary
+    hp.deepseq
+  ]);
+in
 pkgs.stdenv.mkDerivation {
   pname = "fls-shake";
   version = "0.1.0.0";
 
   src = ./.;
 
-  buildInputs = with pkgs.haskellPackages; [
-    ghc
-    shake
-    hashable
-    binary
-    deepseq
-  ];
+  nativeBuildInputs = [ ghcEnv ];
 
   buildPhase = ''
     ghc -o fls-shake Shakefile.hs -threaded
