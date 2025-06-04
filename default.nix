ce8c8074b356d18dded8b629dc6d9c7d6ecc6ed3
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-06-04 16:58:21 +0200

    Port agda deps. to use niv

--- default.nix
+++ default.nix
@@ -25,12 +25,7 @@ let
     inherit (locales) LANG LC_ALL LOCALE_ARCHIVE;
     pname = "agda-stdlib-classes";
     version = "2.2.+";
-    src = fetchFromGitHub {
-      repo = "agda-stdlib-classes";
-      owner = "agda";
-      rev = "aa62ce6348d39c554ef89487079871d5590e155e";
-      sha256 = "sha256-I/g0BOdeAHVEtsfmPBICySOd6Jz5ymGUSE/G66EfHK8=";
-    };
+    src = sources.agda-stdlib-classes;
     meta = { };
     libraryFile = "agda-stdlib-classes.agda-lib";
     everythingFile = "standard-library-classes.agda";
@@ -41,12 +36,7 @@ let
     inherit (locales) LANG LC_ALL LOCALE_ARCHIVE;
     pname = "agda-stdlib-meta";
     version = "2.2.+";
-    src = fetchFromGitHub {
-      repo = "agda-stdlib-meta";
-      owner = "agda";
-      rev = "5ff853375180ef69f243ce72f2d3f6294bdb6aff";
-      sha256 = "sha256-CNKEnDUToKEv+6Gaa8p5igLNpQDuasQ01JJLOXcU1bA=";
-    };
+    src = sources.agda-stdlib-meta;
     meta = { };
     libraryFile = "agda-stdlib-meta.agda-lib";
     everythingFile = "standard-library-meta.agda";
@@ -57,12 +47,7 @@ let
     inherit (locales) LANG LC_ALL LOCALE_ARCHIVE;
     pname = "agda-sets";
     version = "";
-    src = fetchFromGitHub {
-      repo = "agda-sets";
-      owner = "input-output-hk";
-      rev = "f517d0d0c1ff1fd6dbac8b34309dea0e1aea6fc6";
-      sha256 = "sha256-OsdDNNJp9NWDgDM0pDOGv98Z+vAS1U8mORWF7/B1D7k=";
-    };
+    src = sources.agda-sets;
     meta = { };
     libraryFile = "abstract-set-theory.agda-lib";
     everythingFile = "src/abstract-set-theory.agda";
@@ -73,12 +58,7 @@ let
     pname = "iog-prelude";
     version = "+";
     meta = { };
-    src = fetchFromGitHub {
-      repo = "iog-agda-prelude";
-      owner = "input-output-hk";
-      rev = "20e4ab42fd6a980233053c8c3b1b8b2ab42946c9";
-      sha256 = "sha256-PvfxcoK5MweXfdtbfDUTY23xsaAG093MbeX9fRac4sQ=";
-    };
+    src = sources.iog-agda-prelude;
     libraryFile = "iog-prelude.agda-lib";
     everythingFile = "src/Everything.agda";
     buildInputs = [ agdaStdlib agdaStdlibClasses ];
