7fcac1b47de76c485681ffc4d0ce6d233d84ccb5
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-06-09 11:33:32 +0200

    Restrict systems it builds to x86_64

--- flake.nix
+++ flake.nix
@@ -9,8 +9,9 @@
     let
       # reuse the dependencies pinned by niv
       sources = import ./build-tools/nix/sources.nix;
+      systems = ["x86_64-linux"];
     in
-      flake-utils.lib.eachDefaultSystem (system:
+      flake-utils.lib.eachSystem systems (system:
         let
           nixpkgs = import sources.nixpkgs { inherit system; };
           packageSet = import ./default.nix { inherit nixpkgs; };
