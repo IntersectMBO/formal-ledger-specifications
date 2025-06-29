1abdb4a5d13a38b5b82dfa39cbdc5d1521134b5c
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-06-29 23:25:42 +0200

    Improve ci (#807)
    * Add formalLedger as a buildInput
    
    * Inherit all derivations
    
    * Add custom mkDerivation
    
    * Add project to package name for pdfs
    
    * Update name of target in gh action
    
    * Rename hsSrc (for consistency)
    
    * Remove build-support; format
    
    * Uncomment section; fix imports
    
    * Add mkdocs derivation; use it in gh action
    
    * Fix CI
    
    * Update README

--- build-tools/agda/nix/fls-agda.nix
+++ build-tools/agda/nix/fls-agda.nix
@@ -1,3 +1,16 @@
-{ sources ? import ../../nix/sources.nix
-, nixpkgs ? import sources.nixpkgs { }
-}: nixpkgs.haskellPackages.callCabal2nix "fls-agda" ../. { }
+{
+  sources ? import ../../nix/sources.nix,
+  nixpkgs ? import sources.nixpkgs { },
+}:
+let
+  fls-agda-override = oldAttrs: {
+    outputs = [ "bin" ] ++ (oldAttrs.outputs or [ ]);
+    postInstall =
+      (oldAttrs.postInstall or "")
+      + ''
+        mkdir -p $bin/bin
+        mv $out/bin/fls-agda $bin/bin/agda
+      '';
+  };
+in
+(nixpkgs.haskellPackages.callCabal2nix "fls-agda" ../. { }).overrideAttrs fls-agda-override
--- flake.nix
+++ flake.nix
@@ -5,28 +5,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, ... }:
+  outputs =
+    { self, flake-utils, ... }:
     let
       # reuse the dependencies pinned by niv
       sources = import ./build-tools/nix/sources.nix;
-      systems = ["x86_64-linux"];
+      systems = [ "x86_64-linux" ];
     in
-      flake-utils.lib.eachSystem systems (system:
-        let
-          nixpkgs = import sources.nixpkgs { inherit system; };
-          packageSet = import ./default.nix { inherit nixpkgs; };
-        in {
-          packages = packageSet // {
-            # Set default package
-            default = packageSet.formalLedger;
-          };
-          # Expose development shells
-          devShells = packageSet.devShells // {
-            # default shell points to the main development environment
-            default = packageSet.devShells.default;
-          };
+    flake-utils.lib.eachSystem systems (
+      system:
+      let
+        nixpkgs = import sources.nixpkgs { inherit system; };
+        packageSet = import ./default.nix { inherit nixpkgs; };
+      in
+      {
+        packages = packageSet // {
+          # Set default package
+          default = packageSet.formalLedger;
+        };
+        # Expose development shells
+        devShells = packageSet.devShells // {
+          # default shell points to the main development environment
+          default = packageSet.devShells.default;
+        };
 
-          # Keep hydraJobs for CI
-          hydraJobs = packageSet;
-      });
+        # Keep hydraJobs for CI
+        hydraJobs = packageSet;
+      }
+    );
 }
