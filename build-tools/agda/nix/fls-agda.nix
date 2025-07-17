80afd2855f686e07064da5473b1957290d2beaac
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-07-17 18:24:30 +0200

    Add remove-references-to to fls-agda derivation

--- build-tools/agda/nix/fls-agda.nix
+++ build-tools/agda/nix/fls-agda.nix
@@ -4,13 +4,24 @@
 }:
 let
   fls-agda-override = oldAttrs: {
+    nativeBuildInputs = [ nixpkgs.removeReferencesTo ] ++ (oldAttrs.nativeBuildInputs or [ ]);
     outputs = [ "bin" ] ++ (oldAttrs.outputs or [ ]);
     postInstall =
       (oldAttrs.postInstall or "")
       + ''
+        remove-references-to -t ${nixpkgs.haskellPackages.Agda}         $out/bin/agda
         mkdir -p $bin/bin
-        mv $out/bin/fls-agda $bin/bin/agda
+        cp $out/bin/fls-agda $bin/bin/agda
       '';
   };
 in
 (nixpkgs.haskellPackages.callCabal2nix "fls-agda" ../. { }).overrideAttrs fls-agda-override
+# remove-references-to -t ${nixpkgs.haskellPackages.deepseq}      $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.blaze-html}   $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.containers}   $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.uri-encode}   $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.text}         $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.filepath}     $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.split}        $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.transformers} $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.mtl}          $out/bin/agda
