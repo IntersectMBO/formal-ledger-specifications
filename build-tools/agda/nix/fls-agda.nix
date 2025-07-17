b07f5279f477949bd7880c487eb0d5d4ad29423f
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-07-17 18:37:44 +0200

    Fix path of exe

--- build-tools/agda/nix/fls-agda.nix
+++ build-tools/agda/nix/fls-agda.nix
@@ -9,19 +9,21 @@ let
     postInstall =
       (oldAttrs.postInstall or "")
       + ''
-        remove-references-to -t ${nixpkgs.haskellPackages.Agda}         $out/bin/agda
+        remove-references-to -t ${nixpkgs.haskellPackages.Agda}         $out/bin/fls-agda
+        remove-references-to -t ${nixpkgs.haskellPackages.blaze-html}   $out/bin/fls-agda
+        remove-references-to -t ${nixpkgs.haskellPackages.uri-encode}   $out/bin/fls-agda
         mkdir -p $bin/bin
         cp $out/bin/fls-agda $bin/bin/agda
       '';
   };
 in
 (nixpkgs.haskellPackages.callCabal2nix "fls-agda" ../. { }).overrideAttrs fls-agda-override
-# remove-references-to -t ${nixpkgs.haskellPackages.deepseq}      $out/bin/agda
-# remove-references-to -t ${nixpkgs.haskellPackages.blaze-html}   $out/bin/agda
+
+# Errors:
 # remove-references-to -t ${nixpkgs.haskellPackages.containers}   $out/bin/agda
-# remove-references-to -t ${nixpkgs.haskellPackages.uri-encode}   $out/bin/agda
-# remove-references-to -t ${nixpkgs.haskellPackages.text}         $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.deepseq}      $out/bin/agda
 # remove-references-to -t ${nixpkgs.haskellPackages.filepath}     $out/bin/agda
 # remove-references-to -t ${nixpkgs.haskellPackages.split}        $out/bin/agda
-# remove-references-to -t ${nixpkgs.haskellPackages.transformers} $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.text}         $out/bin/agda
 # remove-references-to -t ${nixpkgs.haskellPackages.mtl}          $out/bin/agda
+# remove-references-to -t ${nixpkgs.haskellPackages.transformers} $out/bin/agda
