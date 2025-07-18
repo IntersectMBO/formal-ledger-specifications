58074ccec0ea903b464f06ab638df13b61289325
Robin Stumm <robin.stumm@iohk.io>
2025-07-18 15:43:06 +0200

    add aggregate hydra job over all systems

--- flake.nix
+++ flake.nix
@@ -11,43 +11,57 @@
       # reuse the dependencies pinned by niv
       sources = import ./build-tools/nix/sources.nix;
       systems = [ "x86_64-linux" ];
-    in
-    flake-utils.lib.eachSystem systems (
-      system:
-      let
-        nixpkgs = import sources.nixpkgs { inherit system; };
-        pkgs = import ./default.nix { inherit nixpkgs; };
-      in
-      {
-        packages = pkgs // {
-          # Set default package
-          default = pkgs.formal-ledger;
-        };
-        # Expose development shells
-        devShells = pkgs.devShells // {
-          # default shell points to the main development environment
-          default = pkgs.devShells.default;
-        };
 
-        # Keep hydraJobs for CI
-        hydraJobs =
-          let
-            jobs = {
-              inherit (pkgs)
-                agdaWithPackages
-                fls-shake
-                formal-ledger
-                hs-src
-                mkdocs
-                ;
-            };
-          in
-          jobs // {
-            required = nixpkgs.releaseTools.aggregate {
-              name = "${system}-required";
-              constituents = builtins.attrValues jobs;
-            };
+      perSystem = flake-utils.lib.eachSystem systems (
+        system:
+        let
+          nixpkgs = import sources.nixpkgs { inherit system; };
+          pkgs = import ./default.nix { inherit nixpkgs; };
+        in
+        {
+          packages = pkgs // {
+            # Set default package
+            default = pkgs.formal-ledger;
           };
-      }
-    );
+          # Expose development shells
+          devShells = pkgs.devShells // {
+            # default shell points to the main development environment
+            default = pkgs.devShells.default;
+          };
+
+          # Keep hydraJobs for CI
+          hydraJobs =
+            let
+              jobs = {
+                inherit (pkgs)
+                  agdaWithPackages
+                  fls-shake
+                  formal-ledger
+                  hs-src
+                  mkdocs
+                  ;
+              };
+            in
+            jobs // {
+              required = nixpkgs.releaseTools.aggregate {
+                name = "${system}-required";
+                constituents = builtins.attrValues jobs;
+              };
+            };
+        }
+      );
+    in
+    perSystem
+    // {
+      hydraJobs = perSystem.hydraJobs // {
+        required =
+          let
+            nixpkgs = import sources.nixpkgs { system = builtins.head systems; };
+          in
+          nixpkgs.releaseTools.aggregate {
+            name = "required";
+            constituents = map (system: perSystem.hydraJobs.${system}.required) systems;
+          };
+      };
+    };
 }
