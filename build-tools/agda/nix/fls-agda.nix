2c12c72d9c786edf695d7745024ed660825d075b
Veronika Romashkina <vrom911@gmail.com>
2025-07-21 22:43:12 +0100

    Fix build on Mac

--- build-tools/agda/nix/fls-agda.nix
+++ build-tools/agda/nix/fls-agda.nix
@@ -2,15 +2,8 @@
   sources ? import ../../nix/sources.nix,
   nixpkgs ? import sources.nixpkgs { },
 }:
-let
-  fls-agda-override = oldAttrs: {
-    outputs = [ "bin" ] ++ (oldAttrs.outputs or [ ]);
-    postInstall =
-      (oldAttrs.postInstall or "")
-      + ''
-        mkdir -p $bin/bin
-        mv $out/bin/fls-agda $bin/bin/agda
-      '';
-  };
-in
-(nixpkgs.haskellPackages.callCabal2nix "fls-agda" ../. { }).overrideAttrs fls-agda-override
+(nixpkgs.haskellPackages.callCabal2nix "fls-agda" ../. { }).overrideAttrs (oldAttrs: {
+  postInstall = (oldAttrs.postInstall or "") + ''
+    ln -sf $out/bin/fls-agda $out/bin/agda
+  '';
+})
