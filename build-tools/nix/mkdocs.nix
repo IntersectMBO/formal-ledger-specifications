b9aaf047bd4c0d7aabbede98a03431751276f567
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2026-02-20 11:09:12 +0100

    Adjust nix derivations to use fls-shake

--- build-tools/nix/mkdocs.nix
+++ build-tools/nix/mkdocs.nix
@@ -1,22 +1,18 @@
 {
   lib,
+  stdenv,
+  glibcLocales,
   mkDerivation,
-  fls-agdaWithPackages,
-  dejavu_fonts,
-  ghostscript,
-  pandoc,
-  python3,
-  texlive,
 }:
 mkDerivation {
-  name = "mkdocs";
-  version = "0.1";
+  LANG = "en_US.UTF-8";
+  LC_ALL = "en_US.UTF-8";
+  LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
+  pname = "mkdocs";
   src = lib.fileset.toSource {
     root = ../../.;
     fileset = lib.fileset.unions [
       ../../README.md
-      ../../CONTRIBUTING.md
-      ../scripts
       ../static/md/mkdocs
       ../static/latex
       ../../src
@@ -24,50 +20,19 @@ mkDerivation {
       ../../src-lib-exts
     ];
   };
-  meta = { };
-  buildInputs = [
-    dejavu_fonts
-    fls-agdaWithPackages
-    ghostscript
-    pandoc
-    (python3.withPackages (
-      ps: with ps; [
-        pip
-        mkdocs
-        mkdocs-material
-        pymdown-extensions
-        pyyaml
-        pybtex
-      ]
-    ))
-    (texlive.combine {
-      inherit (texlive)
-        bclogo
-        scheme-small
-        dejavu
-        mdframed
-        zref
-        luatex
-        luatex85
-        dvisvgm
-        needspace
-        standalone
-        tikz-cd
-        pgfplots
-        ;
-    })
-  ];
   buildPhase = ''
     runHook preBuild
-    mkdir -p dist
-    env HOME=$(mktemp -d) python ./build-tools/scripts/md/build.py --run-agda
-    (cd _build/md/mkdocs/; mkdocs build --site-dir ../../../dist/site)
+
+    fls-shake --trace mkdocs
+
     runHook postBuild
   '';
   installPhase = ''
     runHook preInstall
-    mkdir -p "$out"
-    cp -r "dist/site" "$out"
+
+    mkdir "$out"
+    cp -r dist/mkdocs "$out"
+
     runHook postInstall
   '';
 }
